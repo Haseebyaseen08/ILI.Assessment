b5ca5b11d672d8324bf2df3ef7847a922422ccfd
Haseeb.Yaseen <Haseeb.Yaseen@HASEEBYASEEN-EN>
2025-09-15 02:40:19 +0500

    Add migrations

---
+++ src/Data/Migrations/20250914213940_MonthlyUsageSumary.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class MonthlyUsageSumary : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MonthlyUsageSummaries",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CustomerId = table.Column<int>(type: "int", nullable: false),
+                    Year = table.Column<int>(type: "int", nullable: false),
+                    Month = table.Column<int>(type: "int", nullable: false),
+                    SubscriptionType = table.Column<int>(type: "int", nullable: false),
+                    TotalApiCalls = table.Column<int>(type: "int", nullable: false),
+                    PricePerCall = table.Column<decimal>(type: "decimal(18,4)", precision: 18, scale: 4, nullable: false),
+                    TotalCost = table.Column<decimal>(type: "decimal(18,4)", precision: 18, scale: 4, nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EndpointUsageJson = table.Column<string>(type: "nvarchar(4000)", maxLength: 4000, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MonthlyUsageSummaries", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_MonthlyUsageSummaries_Customers_CustomerId",
+                        column: x => x.CustomerId,
+                        principalTable: "Customers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MonthlyUsageSummaries_CustomerId_Year_Month",
+                table: "MonthlyUsageSummaries",
+                columns: new[] { "CustomerId", "Year", "Month" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MonthlyUsageSummaries");
+        }
+    }
+}
